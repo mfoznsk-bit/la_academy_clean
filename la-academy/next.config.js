b3e78d66b508432934c5c26a7f2a4f38d3372981
mfoznsk-bit <mfoznsk@gmail.com>
2025-10-03 15:57:44 +0100

    Add next.config.js to ignore TS build errors

---
+++ la-academy/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Ignore TypeScript errors during production build
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
